5d183b7a0be0b74801f7924ab021bf4fb1bf781f
bgeVam <bernold.georg@gmail.com>
2017-06-02 17:54:58 +0200

    Update installation

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-
+# Move sources
 sudo mkdir /opt/pycastr/
 sudo cp -r src/ /opt/pycastr/
-
 sudo cp data/icons/pycastr_* /usr/share/icons/hicolor/256x256/apps/
 sudo cp data/icons/pycastr* /usr/share/icons/hicolor/256x256/apps/
 sudo cp data/icons/pycastr-* /usr/share/icons/hicolor/scalable/apps/
 
 gtk-update-icon-cache
 
-sudo apt-get update
+# Install dependencies
+#sudo apt-get update
 sudo apt-get -y install vlc
 sudo apt-get -y install python3
 sudo apt-get -y install python3-pip
 pip3 install netdisco
 pip3 install kodi-json
 
+# Prepare autostart
 cp pycastr.desktop ~/.config/autostart
